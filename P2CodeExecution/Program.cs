e2e43f544eff73639c4bad50e2a8cc6001a442b1
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-04 16:08:56 +0200

    Created solution

--- P2CodeExecution/Program.cs
+++ P2CodeExecution/Program.cs
@@ -2,10 +2,10 @@
 // The empty space on lines 7 and 8 are ignored since they contain no code, the double command on line 6 
 // runs the two operations as normal since the semicolon ';' indicates the end of a line of code and 
 // thus an operation
+
 Console.WriteLine("This is line 1.");
 Console.WriteLine("This is line 2.");
-Console.WriteLine("This is line 3.");Console.WriteLine("This is line 4.");
+Console.WriteLine("This is line 3.");
+Console.WriteLine("This is line 4.");
 Console.WriteLine("This is line 5.");
-
-
 Console.WriteLine("This is line 6.");
