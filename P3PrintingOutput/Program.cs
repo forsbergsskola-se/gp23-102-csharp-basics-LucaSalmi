675310c96f0d118f2aee7ba45c4443c7f61c9b59
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-04 16:20:15 +0200

    modified to execute on one line

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Welcome to 102 CSharp Basics!");
-Console.WriteLine("It's easy to print output...");
-Console.WriteLine("...to the console!");
+Console.WriteLine("Hello, World! \nWelcome to 102 CSharp Basics! \nIt's easy to print output... \n...to the console!");
