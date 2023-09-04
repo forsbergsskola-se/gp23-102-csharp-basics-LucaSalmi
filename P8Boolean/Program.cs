c17278fd0ee75f700008d70901ce72aa8a358408
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-04 15:13:14 +0200

    completed exercise

--- P8Boolean/Program.cs
+++ P8Boolean/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var isChild = false;
+var isTeenager = false;
+var isAdult = false;
+Console.WriteLine("Write your Age:");
+var ageInput = Console.ReadLine();
+if (int.TryParse(ageInput, out var age))
+{
+    switch (age)
+    {
+        case < 13:
+            isChild = true;
+            break;
+        case > 19:
+            isAdult = true;
+            break;
+        default:
+            isTeenager = true;
+            break;
+    }
+    Console.WriteLine($"You are a Child: {isChild}");
+    Console.WriteLine($"You are a Teenager: {isTeenager}");
+    Console.WriteLine($"You are an Adult: {isAdult}");
+}
+Console.WriteLine("Invalid Input");
+
