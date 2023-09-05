a2441f7dd9bcba9bdf22361e63b5778f65fe5b9b
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 10:29:19 +0200

    created and solved P8Conversion

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+Console.WriteLine("Write a Number");
+string? userInput = Console.ReadLine();
+if (userInput == null)
+{
+    Console.WriteLine("Invalid Input");
+}
+else
+{
+   double convertedToDouble = Convert.ToDouble(userInput);
+   Console.WriteLine($"double from string {convertedToDouble}");
+   int convertedToInt = (int)convertedToDouble;
+   Console.WriteLine($"int from double {convertedToInt}");
+   int number = int.Parse(userInput, CultureInfo.InvariantCulture);
+   Console.WriteLine($"int from string {number}");
+
+}
