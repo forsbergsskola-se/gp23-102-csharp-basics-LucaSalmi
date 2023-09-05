57fa4d19da0b329d551b84008a379e99f2ba46f1
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 09:17:54 +0200

    P6 data types complete

---
+++ P6DataTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string outputString = "Hello World";
+Console.WriteLine(outputString);
+double decimalValue = 0.3;
+Console.WriteLine(decimalValue);
+bool isAwesome = true;
+Console.WriteLine(isAwesome);
+byte hexByte = 0xa7;
+Console.WriteLine(hexByte);
+
+
