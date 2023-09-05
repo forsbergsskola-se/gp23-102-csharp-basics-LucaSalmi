313ca50d62753b2eda5dad72234d0214053107c6
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 10:29:39 +0200

    created P9Operators

---
+++ P9Operators/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write an amount of seconds:");
+var userInput = Console.ReadLine();
+if (int.TryParse(userInput, out var totalSeconds))
+{
+    var seconds = totalSeconds % 60;
+    var minutes = totalSeconds % 60;
+    var hours = totalSeconds % 24;
+    var days = totalSeconds % 60;
+    
+};
