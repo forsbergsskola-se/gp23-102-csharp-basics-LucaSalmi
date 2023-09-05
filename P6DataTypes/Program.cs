16a1ede8009d842596cb4de4b4ecaf99eadfdb5e
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 14:38:59 +0200

    fixed errors

--- P6DataTypes/Program.cs
+++ P6DataTypes/Program.cs
@@ -1,11 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string outputString = "Hello World";
-Console.WriteLine(outputString);
-double decimalValue = 0.3;
-Console.WriteLine(decimalValue);
+float decimalValue = 0.3f;
 bool isAwesome = true;
-Console.WriteLine(isAwesome);
 byte hexByte = 0xa7;
-Console.WriteLine(hexByte);
 
-
+Console.WriteLine($"""
+                   {outputString}
+                   {decimalValue}
+                   {isAwesome}
+                   {hexByte}
+                   """);
