14e234b20df05fc10fc31df65b2f552ffe6d0330
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 14:39:39 +0200

    completed P9 Operators

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -4,9 +4,18 @@ Console.WriteLine("Write an amount of seconds:");
 var userInput = Console.ReadLine();
 if (int.TryParse(userInput, out var totalSeconds))
 {
-    var seconds = totalSeconds % 60;
-    var minutes = totalSeconds % 60;
-    var hours = totalSeconds % 24;
-    var days = totalSeconds % 60;
-    
+    var hours = (totalSeconds / 3600);
+    var minutes = (totalSeconds % 3600) / 60;
+    var seconds = totalSeconds % (minutes * 60);
+    var days = hours / 24;
+    var hoursToDisplay = hours - (24 * days);
+    var daysTotal = totalSeconds / (24 * 60 * 60f);
+    Console.WriteLine($"""
+                       seconds: {seconds}
+                       minutes: {minutes},
+                       hours: {hoursToDisplay},
+                       days: {days},
+                       """);
+    Console.WriteLine($"{days}.{hoursToDisplay}:{minutes}:{seconds}");
+    Console.WriteLine($"In total it's {daysTotal} days");
 };
