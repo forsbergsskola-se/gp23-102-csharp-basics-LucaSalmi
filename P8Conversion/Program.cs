833847d4e736e682cd37bfb8ba2ee77e396ac808
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 15:32:12 +0200

    P9 if else completed

--- P8Conversion/Program.cs
+++ P8Conversion/Program.cs
@@ -5,18 +5,10 @@ using System.Globalization;
 Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
 
 Console.WriteLine("Write a Number");
-string? userInput = Console.ReadLine();
-if (userInput == null)
-{
-    Console.WriteLine("Invalid Input");
-}
-else
-{
-   double convertedToDouble = Convert.ToDouble(userInput);
-   Console.WriteLine($"double from string {convertedToDouble}");
-   int convertedToInt = (int)convertedToDouble;
-   Console.WriteLine($"int from double {convertedToInt}");
-   int number = int.Parse(userInput);
-   Console.WriteLine($"int from string {number}");
-
-}
+string? userInput = Console.ReadLine()!;
+double convertedToDouble = Convert.ToDouble(userInput);
+Console.WriteLine($"double from string {convertedToDouble}");
+int convertedToInt = (int)convertedToDouble;
+Console.WriteLine($"int from double {convertedToInt}");
+int number = Convert.ToInt32(userInput);
+Console.WriteLine($"int from string {number}");
---
+++ P9IfElse/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Write your Age:");
+var ageInput = Console.ReadLine();
+if (int.TryParse(ageInput, out var age))
+{
+    switch (age)
+    {
+        case < 13:
+            Console.WriteLine($"You are a Child");
+            break;
+        case > 19:
+            Console.WriteLine($"You are an Adult");
+            break;
+        default:
+            Console.WriteLine($"You are a Teenager");
+            break;
+    }
+    
+    Console.WriteLine("Give me another number");
+    var secondInput = Console.ReadLine()!;
+    if (int.TryParse(secondInput, out var secondValue))
+    {
+        Console.WriteLine(secondValue > age
+            ? "This number is higher then your age"
+            : "This number is lower then your age");
+        
+        Console.WriteLine(secondValue % 2 == 1
+            ? "The number is odd"
+            : "The number is even");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
