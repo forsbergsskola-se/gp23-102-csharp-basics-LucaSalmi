be4c0f157fabeff429f41678b259fa6319189558
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-05 10:28:46 +0200

    added alt solution

--- P3-Printing-Output/Program.cs
+++ P3-Printing-Output/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!\nWelcome to 102 CSharp Basics!\nIt's easy to print output...\n...to the console!");
+Console.WriteLine("""
+                  Hello, World!
+                  Welcome to 102 CSharp Basics!
+                  It's easy to print output...
+                  ...to the console!
+                  """);
