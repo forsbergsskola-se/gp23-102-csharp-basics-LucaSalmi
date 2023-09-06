db4198b2a2d56711803c066b459244c764886d9d
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-06 13:52:58 +0200

    P9 Completed

--- P9Operators/Program.cs
+++ P9Operators/Program.cs
@@ -4,18 +4,19 @@ Console.WriteLine("Write an amount of seconds:");
 var userInput = Console.ReadLine();
 if (int.TryParse(userInput, out var totalSeconds))
 {
-    var hours = (totalSeconds / 3600);
-    var minutes = (totalSeconds % 3600) / 60;
-    var seconds = totalSeconds % (minutes * 60);
+    var hours = (totalSeconds / 3600);  // TOT / by number of seconds in a day
     var days = hours / 24;
-    var hoursToDisplay = hours - (24 * days);
-    var daysTotal = totalSeconds / (24 * 60 * 60f);
+    hours -= (24 * days);
+    var minutes = (totalSeconds % 3600) / 60;  // (TOT reminder of number of seconds in a day) / number of minutes in an hour
+    var seconds = totalSeconds % (minutes * 60); // TOT % (minutes * seconds in a minute)
+    var daysTotal = totalSeconds / (24 * 3600f); // TOT / (hours in a day * seconds in a day) Float for precision
+    
     Console.WriteLine($"""
                        seconds: {seconds}
                        minutes: {minutes},
-                       hours: {hoursToDisplay},
+                       hours: {hours},
                        days: {days},
                        """);
-    Console.WriteLine($"{days}.{hoursToDisplay}:{minutes}:{seconds}");
+    Console.WriteLine($"{days}.{hours}:{minutes}:{seconds}");
     Console.WriteLine($"In total it's {daysTotal} days");
 };
---
+++ P9_1-km-converter/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.WriteLine("""
+                  Welcome to the km/h to m/s converter!
+                  Enter a value in km/h
+                  """);
+var userInput = Console.ReadLine();
+var kmHValue = float.Parse(userInput!);
+var milesPerSecValue = (kmHValue / 3600) * 1000;
+Console.WriteLine($"You are going at {milesPerSecValue} m/s");
+// km/h * 1000
+// result / 3600
+*/
+
+Console.WriteLine("""
+                  Welcome to the minutes to seconds converter!
+                  Enter a value:
+                  """);
+var userInput = Console.ReadLine();
+var minutes = int.Parse(userInput!);
+var seconds = minutes * 60;
+Console.WriteLine($"{minutes} minutes is {seconds} seconds in total");
