a45fc88488786b4dc13d6f25d49217a0aaae4fec
LucaSalmi <luca.salmi@student.forsbergsskola.se>
2023-09-06 09:55:30 +0200

    P10 Random complete

---
+++ P10Random/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please pass me a seed(integer)");
+var seed = Console.ReadLine();
+Random randomGenerator = new Random(int.Parse(seed!));
+Console.WriteLine($"""
+                   Three numbers between 0 and 5:
+                   {randomGenerator.Next(0,5)},
+                   {randomGenerator.Next(0,5)},
+                   {randomGenerator.Next(0,5)}, 
+                   """);
+Console.WriteLine($"""
+                   Three numbers between 0 and 0.5:
+                   {randomGenerator.NextDouble()*0.5},
+                   {randomGenerator.NextDouble()*0.5},
+                   {randomGenerator.NextDouble()*0.5},
+                   """);
+Console.WriteLine($"""
+                   Three numbers between 0.2 and 0.7:
+                   {randomGenerator.NextDouble()*(0.7-0.2)+0.2},
+                   {randomGenerator.NextDouble()*(0.7-0.2)+0.2},
+                   {randomGenerator.NextDouble()*(0.7-0.2)+0.2},
+                   """);
+Console.WriteLine("Give me a crit chance between 0,0 and 1,0)");
+var critChance = Console.ReadLine();
+
+for (var i = 0; i < 3; i++)
+{
+    if (randomGenerator.NextDouble() > double.Parse(critChance!))
+    {
+        Console.WriteLine("No Crit");
+    }
+    else
+    {
+        Console.WriteLine("Crit");
+    }
+}
+
